fb894c6f4256821ae65ef297beb137cef23675a6
Qotne <cqeshelashvili@gmail.com>
2025-02-16 11:20:05 +0400

    commit\

---
+++ day35/homework/lesson0.py
@@ -0,0 +1,26 @@
+print(7425 / 550)
+
+bill_amount = float(input())
+tip = bill_amount * 0.2
+print(tip)
+
+weight = float(input())
+height = float(input())
+
+bmi = weight / (height ** 2)
+
+if bmi < 18.5:
+    print("Underweight")
+elif 18.5 <= bmi < 25:
+    print("Normal")
+elif 25 <= bmi < 30:
+    print("Overweight")
+else:
+    print("Obesity")
+
+    def search(text, word):
+    if word in text:
+        result = "Word found"
+    else:
+        result = "Word not found"
+    return result
