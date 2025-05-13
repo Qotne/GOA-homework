2aeed5380eaab0f702e70140a6daf542b30b90da
Qotne <cqeshelashvili@gmail.com>
2025-05-13 18:31:32 +0400

    commit

---
+++ day61/home/script.js
@@ -0,0 +1,11 @@
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+  sum += i; 
+}
+
+console.log("ჯამი:", sum);
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i + " - კვადრატი: " + (i * i));
+}
---
+++ day62/home/script.js
@@ -0,0 +1,6 @@
+function countdown() {
+  for (let i = 100; i >= 0; i--) {
+    console.log("დარჩენილია " + i + " წამი");
+  }
+}
+countdown();
